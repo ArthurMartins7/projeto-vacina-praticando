d9318142c4f6ca2deaa63042e8d3a066697cd5e1
ArthurMartins7 <showzin048@gmail.com>
2024-05-02 17:51:16 -0300

    testando o conceito de componentes e navegação de dados.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VacinaListagemComponent } from "./vacina-listagem/vacina-listagem.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, VacinaListagemComponent]
 })
 export class AppComponent {
   title = 'projeto-vacina-praticando';
